df0dd7b878e62291a08e21f3bde00dae1a6ce979
lhoriman <lhoriman@79e2b5ea-dad9-11de-b64b-7d26b27941e2>
2010-01-11 23:50:08 +0000

    Brought back all the static methods on ObjectifyService.
    git-svn-id: https://objectify-appengine.googlecode.com/svn/trunk@142 79e2b5ea-dad9-11de-b64b-7d26b27941e2

--- src/com/googlecode/objectify/ObjectifyService.java
+++ src/com/googlecode/objectify/ObjectifyService.java
@@ -1,11 +1,13 @@
 package com.googlecode.objectify;
 
+import com.google.appengine.api.datastore.Transaction;
+
 
 /**
- * <p>This is a simple container for a single static instance of ObjectifyFactory.</p>
- * 
- * <p>You can choose to use this class or build your own equivalent.  You may prefer
- * to use a Dependency Injection system like Weld or Guice.</p>
+ * <p>This is a simple container for a single static instance of ObjectifyFactory.
+ * You can choose to use this class or build your own equivalent.  You may prefer
+ * to use a dependency injection system like Weld or Guice directly with an
+ * ObjectifyFactory and eschew this class entirely.</p>
  * 
  * <p>For further advice, see the
  * <a href="http://code.google.com/p/objectify-appengine/wiki/BestPractices">BestPractices</a>.</p>
@@ -19,4 +21,55 @@ public class ObjectifyService
 	
 	/** Call this to get the instance */
 	public static ObjectifyFactory factory() { return factory; }
+
+	//
+	// All static methods simply pass-through to the singleton factory
+	//
+
+	/** @see ObjectifyFactory#begin() */
+	public static Objectify begin() { return factory().begin(); }
+	
+	/** @see ObjectifyFactory#beginTransaction() */
+	public static Objectify beginTransaction() { return factory().beginTransaction(); }
+	
+	/** @see ObjectifyFactory#withTransaction(Transaction) */
+	public static Objectify withTransaction(Transaction txn) { return factory().withTransaction(txn); }
+
+	/** @see ObjectifyFactory#register(Class) */
+	public static void register(Class<?> clazz) { factory().register(clazz); }
+	
+	/** @see ObjectifyFactory#setDatastoreTimeoutRetryCount(int) */
+	public static void setDatastoreTimeoutRetryCount(int value) { factory().setDatastoreTimeoutRetryCount(value); }
+
+	/** @see ObjectifyFactory#getDatastoreTimeoutRetryCount() */
+	public static int getDatastoreTimeoutRetryCount() { return factory().getDatastoreTimeoutRetryCount(); }
+	
+	//
+	// Methods equivalent to those on KeyFactory, but which use typed Classes instead of kind.
+	//
+	
+	/** @see ObjectifyFactory#createKey(Class, long) */
+	public static <T> OKey<T> createKey(Class<T> kind, long id) { return factory().createKey(kind, id); }
+	
+	/** @see ObjectifyFactory#createKey(Class, String) */
+	public static <T> OKey<T> createKey(Class<T> kind, String name) { return factory().createKey(kind, name); }
+	
+	/** @see ObjectifyFactory#createKey(OKey, Class, long) */
+	public static <T> OKey<T> createKey(OKey<?> parent, Class<T> kind, long id) { return factory().createKey(parent, kind, id); }
+	
+	/** @see ObjectifyFactory#createKey(OKey, Class, String) */
+	public static <T> OKey<T> createKey(OKey<?> parent, Class<T> kind, String name) { return factory().createKey(parent, kind, name); }
+	
+	/** @see ObjectifyFactory#createKey(Object) */
+	public static <T> OKey<T> createKey(T entity) { return factory().createKey(entity); }
+	
+	//
+	// Friendly query creation methods
+	//
+	
+	/** @see ObjectifyFactory#createQuery() */
+	public static <T> OQuery<T> createQuery() { return factory().createQuery(); }
+	
+	/** @see ObjectifyFactory#createQuery(Class) */
+	public static <T> OQuery<T> createQuery(Class<T> entityClazz) { return factory().createQuery(entityClazz); }
 }
