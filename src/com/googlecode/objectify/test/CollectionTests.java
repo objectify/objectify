1e9f86325cd6fd133bcf5625ea926fe417551a88
spudbean@gmail.com <spudbean@gmail.com@79e2b5ea-dad9-11de-b64b-7d26b27941e2>
2010-01-31 02:39:32 +0000

    Clarify null/empty Collection tests, according to these (tentative) rules: Rule: never store an empty Collection, and always reconstruct an empty List. Rule: never store an empty Collection, and always reconstruct an empty List.
    Also, fix up double ;; at end of some lines
    
    git-svn-id: https://objectify-appengine.googlecode.com/svn/trunk@229 79e2b5ea-dad9-11de-b64b-7d26b27941e2

--- src/com/googlecode/objectify/test/CollectionTests.java
+++ src/com/googlecode/objectify/test/CollectionTests.java
@@ -12,8 +12,10 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.TreeSet;
 
+import com.google.appengine.api.datastore.Entity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
@@ -26,7 +28,7 @@ import com.googlecode.objectify.test.entity.HasCollections.CustomSet;
 
 /**
  * Tests of various collection types
- * 
+ *
  * @author Jeff Schnitzer <jeff@infohazard.org>
  */
 public class CollectionTests extends TestBase
@@ -34,12 +36,12 @@ public class CollectionTests extends TestBase
 	/** */
 	@SuppressWarnings("unused")
 	private static Logger log = LoggerFactory.getLogger(CollectionTests.class);
-	
+
 	/** */
 	private void assertContains123(Collection<Integer> coll, Class<?> expectedClass)
 	{
 		assert coll.getClass() == expectedClass;
-		
+
 		assert coll.size() == 3;
 		Iterator<Integer> it = coll.iterator();
 		assert it.next() == 1;
@@ -55,18 +57,18 @@ public class CollectionTests extends TestBase
 
 		HasCollections hc = new HasCollections();
 		hc.integerList = Arrays.asList(1, 2, 3);
-		
+
 		hc.integerArrayList = new ArrayList<Integer>(hc.integerList);
 		hc.integerLinkedList = new LinkedList<Integer>(hc.integerList);
-		
+
 		Key<HasCollections> key = ofy.put(hc);
 		hc = ofy.get(key);
-		
-		assertContains123(hc.integerList, ArrayList.class);;
-		assertContains123(hc.integerArrayList, ArrayList.class);;
-		assertContains123(hc.integerLinkedList, LinkedList.class);;
+
+		assertContains123(hc.integerList, ArrayList.class);
+		assertContains123(hc.integerArrayList, ArrayList.class);
+		assertContains123(hc.integerLinkedList, LinkedList.class);
 	}
-	
+
 	/** */
 	@Test
 	public void testBasicSets() throws Exception
@@ -78,22 +80,22 @@ public class CollectionTests extends TestBase
 		hc.integerSet.add(1);
 		hc.integerSet.add(2);
 		hc.integerSet.add(3);
-		
+
 		hc.integerSortedSet = new TreeSet<Integer>(hc.integerSet);
 		hc.integerHashSet = new HashSet<Integer>(hc.integerSet);
 		hc.integerTreeSet = new TreeSet<Integer>(hc.integerSet);
 		hc.integerLinkedHashSet = new LinkedHashSet<Integer>(hc.integerSet);
-		
+
 		Key<HasCollections> key = ofy.put(hc);
 		hc = ofy.get(key);
-		
-		assertContains123(hc.integerSet, HashSet.class);;
-		assertContains123(hc.integerSortedSet, TreeSet.class);;
-		assertContains123(hc.integerHashSet, HashSet.class);;
-		assertContains123(hc.integerTreeSet, TreeSet.class);;
-		assertContains123(hc.integerLinkedHashSet, LinkedHashSet.class);;
+
+		assertContains123(hc.integerSet, HashSet.class);
+		assertContains123(hc.integerSortedSet, TreeSet.class);
+		assertContains123(hc.integerHashSet, HashSet.class);
+		assertContains123(hc.integerTreeSet, TreeSet.class);
+		assertContains123(hc.integerLinkedHashSet, LinkedHashSet.class);
 	}
-	
+
 	/** */
 	@Test
 	public void testCustomSet() throws Exception
@@ -105,19 +107,19 @@ public class CollectionTests extends TestBase
 		hc.customSet.add(1);
 		hc.customSet.add(2);
 		hc.customSet.add(3);
-		
+
 		Key<HasCollections> key = ofy.put(hc);
 		hc = ofy.get(key);
-		
-		assertContains123(hc.customSet, CustomSet.class);;
+
+		assertContains123(hc.customSet, CustomSet.class);
 	}
-	
+
 	/** */
 	@Test
 	public void testOKeySet() throws Exception
 	{
 		Objectify ofy = this.fact.begin();
-		
+
 		Key<Trivial> key7 = new Key<Trivial>(Trivial.class, 7);
 		Key<Trivial> key8 = new Key<Trivial>(Trivial.class, 8);
 		Key<Trivial> key9 = new Key<Trivial>(Trivial.class, 9);
@@ -127,13 +129,13 @@ public class CollectionTests extends TestBase
 		hc.oKeySet.add(key7);
 		hc.oKeySet.add(key8);
 		hc.oKeySet.add(key9);
-		
+
 		Key<HasCollections> key = ofy.put(hc);
 		hc = ofy.get(key);
-		
+
 		assert hc.oKeySet instanceof HashSet<?>;
 		assert hc.oKeySet.size() == 3;
-		
+
 		Iterator<Key<Trivial>> it = hc.oKeySet.iterator();
 		assert it.next().equals(key7);
 		assert it.next().equals(key8);
@@ -141,32 +143,58 @@ public class CollectionTests extends TestBase
 	}
 
 	@Test
-	public void testNullCollections() throws Exception {
-		// create an entity with a list, save, null it, save and load it
-
+	public void testCollectionContainingNull() throws Exception
+	{
 		Objectify ofy = this.fact.begin();
 
 		HasCollections hc = new HasCollections();
-		hc.integerList = Arrays.asList(1, 2, 3);
+		hc.integerList = Arrays.asList((Integer) null);
 
 		Key<HasCollections> key = ofy.put(hc);
 		hc = ofy.get(key);
 
 		assert hc.integerList != null;
-		assert hc.integerList.size() == 3;
+		assert hc.integerList.size() == 1;
+		assert hc.integerList.get(0) == null;
 
-		assertContains123(hc.integerList, ArrayList.class);
+		Entity e = ofy.getDatastore().get(fact.getRawKey(key));
+		assert e.hasProperty("integerList");
+		List l = (List) e.getProperty("integerList");
+		assert l != null;
+		assert l.size() == 1;
+		assert l.get(0) == null;
+	}
 
+	/**
+	 * Rule: never store a null Collection, and always reconstruct an empty List.
+	 */
+	@Test
+	public void testNullCollections() throws Exception
+	{
+		Objectify ofy = this.fact.begin();
+
+		HasCollections hc = new HasCollections();
 		hc.integerList = null;
+
+		Key<HasCollections> key = ofy.put(hc);
+		hc = ofy.get(key);
 
 		ofy.put(hc);
 		hc = ofy.get(key);
-		assert hc.integerList == null;
+		assert hc.integerList != null;
+		assert hc.integerList.size() == 0;
 
+		Entity e = ofy.getDatastore().get(fact.getRawKey(key));
+		// rule : never store a null collection
+		assert !e.hasProperty("integerList");
 	}
-	
+
+	/**
+	 * Test rule: never store an empty Collection, and always reconstruct an empty List.
+	 */
 	@Test
-	public void testEmptyCollections() throws Exception {
+	public void testEmptyCollections() throws Exception
+	{
 		Objectify ofy = this.fact.begin();
 
 		HasCollections hc = new HasCollections();
@@ -179,5 +207,9 @@ public class CollectionTests extends TestBase
 
 		assert hc.integerList != null;
 		assert hc.integerList.size() == 0;
+
+		Entity e = ofy.getDatastore().get(fact.getRawKey(key));
+		// rule : never store an empty collection
+		assert !e.hasProperty("integerList");
 	}
 }
