7cdd8274dffb3c0b27a88d9fcec15acc6ac39e69
lhoriman <lhoriman@79e2b5ea-dad9-11de-b64b-7d26b27941e2>
2010-04-06 18:10:53 +0000

    Added test of list manipulation in and out of transaction as per issue 30
    git-svn-id: https://objectify-appengine.googlecode.com/svn/trunk@493 79e2b5ea-dad9-11de-b64b-7d26b27941e2

--- src/com/googlecode/objectify/test/TransactionTests.java
+++ src/com/googlecode/objectify/test/TransactionTests.java
@@ -5,12 +5,18 @@
 
 package com.googlecode.objectify.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Id;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
 import com.googlecode.objectify.Key;
 import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.annotation.Cached;
 import com.googlecode.objectify.test.entity.Trivial;
 
 /**
@@ -52,5 +58,43 @@ public class TransactionTests extends TestBase
 		assert fetched.getSomeNumber() == triv.getSomeNumber();
 		assert fetched.getSomeString().equals(triv.getSomeString());
 	}
+	
+	/** */
+	@Cached
+	static class HasSimpleCollection
+	{
+		@Id Long id;
+		List<String> stuff = new ArrayList<String>();
+	}
 
+	/** */
+	@Test
+	public void testInAndOutOfTransaction() throws Exception
+	{
+		this.fact.register(HasSimpleCollection.class);
+		
+		HasSimpleCollection simple = new HasSimpleCollection();
+		
+		Objectify nonTxnOfy = this.fact.begin();
+		nonTxnOfy.put(simple);
+		
+		Objectify txnOfy = this.fact.beginTransaction();
+		HasSimpleCollection simple2;
+		try
+		{
+			simple2 = txnOfy.get(HasSimpleCollection.class, simple.id);
+			simple2.stuff.add("blah");
+			txnOfy.put(simple2);
+			txnOfy.getTxn().commit();
+		}
+		finally
+		{
+			if (txnOfy.getTxn().isActive())
+				txnOfy.getTxn().rollback();
+		}
+		
+		HasSimpleCollection simple3 = nonTxnOfy.get(HasSimpleCollection.class, simple.id);
+		
+		assert simple2.stuff.equals(simple3.stuff);
+	}
 }
