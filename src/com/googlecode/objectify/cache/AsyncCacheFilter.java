4f68c5fec13d6974a310e4f60a896a54ddf47700
latchkey <latchkey@79e2b5ea-dad9-11de-b64b-7d26b27941e2>
2011-05-18 19:02:54 +0000

    add documentation about configuration in the web.xml
    git-svn-id: https://objectify-appengine.googlecode.com/svn/trunk@716 79e2b5ea-dad9-11de-b64b-7d26b27941e2

--- src/com/googlecode/objectify/cache/AsyncCacheFilter.java
+++ src/com/googlecode/objectify/cache/AsyncCacheFilter.java
@@ -12,11 +12,29 @@ import javax.servlet.ServletResponse;
 /**
  * <p>This Filter is a companion to the CachingAsyncDatastoreService, and must be
  * installed any time the CachingAsyncDatastoreService is used.</p>
- * 
+ *
  * <p>This Filter is a temporary measure until Google provides a hook that lets
  * us intercept the raw Future<?> calls at the end of a request.  At that point
  * this filter can be eliminated in favor of the hook.</p>
- * 
+ *
+ * In your web.xml:
+ *<pre>
+ *       &lt;filter&gt;
+ *              &lt;filter-name&gt;AsyncCacheFilter&lt;/filter-name&gt;
+ *              &lt;filter-class&gt;com.googlecode.objectify.cache.AsyncCacheFilter&lt;/filter-class&gt;
+ *      &lt;/filter&gt;
+ *      &lt;filter-mapping&gt;
+ *              &lt;filter-name&gt;AsyncCacheFilter&lt;/filter-name&gt;
+ *              &lt;url-pattern&gt;/*&lt;/url-pattern&gt;
+ *      &lt;/filter-mapping&gt;
+ *</pre>
+ *
+ * Or, if you use Guice:
+ *
+ *<pre>
+ *      filter("/*").through(AsyncCacheFilter.class);
+ *</pre>
+ *
  * @author Jeff Schnitzer <jeff@infohazard.org>
  */
 public class AsyncCacheFilter implements Filter
