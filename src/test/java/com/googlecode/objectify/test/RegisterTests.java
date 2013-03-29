3f02cbc6ecf4abe1f6a390aec815e57eab0507e9
Jeff Schnitzer <jeff@infohazard.org>
2013-03-29 16:12:04 -0400

    Testing what happens when you create a key for an unregistered class

--- src/test/java/com/googlecode/objectify/test/RegisterTests.java
+++ src/test/java/com/googlecode/objectify/test/RegisterTests.java
@@ -2,6 +2,8 @@ package com.googlecode.objectify.test;
 
 import org.testng.annotations.Test;
 
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.annotation.Cache;
 import com.googlecode.objectify.annotation.Embed;
 import com.googlecode.objectify.annotation.Entity;
@@ -10,82 +12,83 @@ import com.googlecode.objectify.test.util.TestBase;
 
 /**
  * Basic tests for registering entities
- * 
+ *
  * @author Matt Quail http://madbean.com/
  */
-public class RegisterTests extends TestBase
-{
-    @Entity
+public class RegisterTests extends TestBase {
+	@Entity
 	@Cache
-	public static class NonPublicConstructor
-	{
-		@Id
-		Long id;
-
+	public static class NonPublicConstructor {
+		@Id Long id;
 		private NonPublicConstructor() { }
 	}
 
-    @Entity
+	@Entity
 	@Cache
-	public static class NoNoargConstructors
-	{
-		@Id
-		Long id;
+	public static class NoNoargConstructors {
+		@Id Long id;
 
-		public NoNoargConstructors(long id)
-		{
+		public NoNoargConstructors(long id) {
 			this.id = id;
 		}
 	}
 
-    public static class BadStruct {
-        int age;
+	public static class BadStruct {
+		int age;
 
-        public BadStruct(int age) {
-            this.age = age;
-        }
-    }
-    
-    @Entity
+		public BadStruct(int age) {
+			this.age = age;
+		}
+	}
+
+	@Entity
 	@Cache
-    public static class HasEmbedded {
-        @Id
-        Long id;
-        @Embed
-        BadStruct name;
+	public static class HasEmbedded {
+		@Id
+		Long id;
+		@Embed
+		BadStruct name;
 
-    }
+	}
+
 
 	@Test
-	public void testNoArgConstructor()
-	{
+	public void testNoArgConstructor() {
 		assertRegisterSucceeds(NonPublicConstructor.class);
 		assertRegisterFails(NoNoargConstructors.class, IllegalStateException.class);
 		assertRegisterFails(HasEmbedded.class, IllegalStateException.class);
 	}
 
-	private void assertRegisterSucceeds(Class<?> entity)
-	{
-		try
-		{
+	private void assertRegisterSucceeds(Class<?> entity) {
+		try {
 			this.fact.register(entity);
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			assert false : "Unexpected exception of type " + e.getClass();
 		}
 	}
 
-	private void assertRegisterFails(Class<?> entity, Class<? extends Exception> expectedException)
-	{
-		try
-		{
+	private void assertRegisterFails(Class<?> entity, Class<? extends Exception> expectedException) {
+		try {
 			this.fact.register(entity);
 			assert false : "Shouldn't be register " + entity.getName();
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			assert expectedException.isInstance(e) : "Unexpected exception of type " + e.getClass();
 		}
+	}
+
+	@Entity
+	@Cache
+	public static class Normal {
+		@Id Long id;
+		String foo;
+	}
+
+	/**
+	 * Checking https://code.google.com/p/objectify-appengine/issues/detail?id=146
+	 */
+	@Test
+	public void makeKeyWithoutRegstering() throws Exception {
+		Objectify ofy = this.fact.begin();
+		ofy.load().key(Key.create(Normal.class, 123L)).get();
 	}
 }
