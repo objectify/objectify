9ca7e3ade47f5972a8ae029312dff7df784e02d4
Jeff Schnitzer <jeff@infohazard.org>
2013-07-10 08:08:33 -0700

    Objectify cache control should ignore any nonregistered kinds

--- src/main/java/com/googlecode/objectify/impl/CacheControlImpl.java
+++ src/main/java/com/googlecode/objectify/impl/CacheControlImpl.java
@@ -4,24 +4,25 @@ import com.google.appengine.api.datastore.Key;
 import com.googlecode.objectify.ObjectifyFactory;
 import com.googlecode.objectify.cache.CacheControl;
 
-/** 
- * Implements CacheControl for Objectify 
+/**
+ * Implements CacheControl for Objectify
  */
 public class CacheControlImpl implements CacheControl
 {
 	/** */
 	ObjectifyFactory fact;
-	
+
 	/** */
 	public CacheControlImpl(ObjectifyFactory fact)
 	{
 		this.fact = fact;
 	}
-	
+
 	/** */
 	@Override
 	public Integer getExpirySeconds(Key key)
 	{
-		return fact.getMetadata(key).getCacheExpirySeconds();
+		EntityMetadata<?> meta = fact.getMetadata(key.getKind());
+		return meta == null ? null : meta.getCacheExpirySeconds();
 	}
 }
