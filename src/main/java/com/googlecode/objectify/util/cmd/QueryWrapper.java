fc3f03d396bbbe6ccb87bae6fc962ac2cfdbaafe
Jeff Schnitzer <jeff@infohazard.org>
2013-07-17 23:29:58 -0700

    Fixed bug with extra QueryWrapper.base

--- src/main/java/com/googlecode/objectify/util/cmd/QueryWrapper.java
+++ src/main/java/com/googlecode/objectify/util/cmd/QueryWrapper.java
@@ -4,35 +4,28 @@ import com.googlecode.objectify.cmd.Query;
 
 /**
  * Simple wrapper/decorator for a Query.  Use it like this:
- * {@code class MyQuery<T> extends QueryWrapper<MyQuery<T>, T>} 
+ * {@code class MyQuery<T> extends QueryWrapper<MyQuery<T>, T>}
  *
  * @author Jeff Schnitzer <jeff@infohazard.org>
  */
 public class QueryWrapper<H extends QueryWrapper<H, T>, T> extends SimpleQueryWrapper<H, T> implements Query<T>
 {
 	/** */
-	Query<T> base;
-	
-	/** */
-	public QueryWrapper(Query<T> base) 
-	{
+	public QueryWrapper(Query<T> base) {
 		super(base);
-		this.base = base;
 	}
-	
+
 	@Override
-	public H filter(String condition, Object value)
-	{
+	public H filter(String condition, Object value) {
 		H next = this.clone();
-		next.base = base.filter(condition, value);
+		next.base = ((Query<T>)base).filter(condition, value);
 		return next;
 	}
-	
+
 	@Override
-	public H order(String condition)
-	{
+	public H order(String condition) {
 		H next = this.clone();
-		next.base = base.order(condition);
+		next.base = ((Query<T>)base).order(condition);
 		return next;
 	}
 }
