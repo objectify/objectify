611fcb420876e652a4a4674628d2cfc58a3b0c55
Huseyn Guliyev <husayt@gmail.com>
2014-02-06 01:40:13 +0000

    * Added reverse Query support (cherry picked from commit 62eab2f)

--- src/main/java/com/googlecode/objectify/impl/SimpleQueryImpl.java
+++ src/main/java/com/googlecode/objectify/impl/SimpleQueryImpl.java
@@ -12,150 +12,147 @@ import com.googlecode.objectify.cmd.SimpleQuery;
  *
  * @author Jeff Schnitzer <jeff@infohazard.org>
  */
-public abstract class SimpleQueryImpl<T> implements SimpleQuery<T>
-{
-	/** */
-	protected LoaderImpl<?> loader;
+public abstract class SimpleQueryImpl<T> implements SimpleQuery<T> {
+    /** */
+    protected LoaderImpl<?> loader;
 
-	/**
-	 * There is a special case - if loader is null, use 'this' as the LoaderImpl. It's a bit of a hack
-	 * but we can't pass in 'this' to super constructors.
-	 */
-	SimpleQueryImpl(LoaderImpl<?> loader) {
-		this.loader = loader == null ? (LoaderImpl<?>)this : loader;
-	}
+    /**
+     * There is a special case - if loader is null, use 'this' as the LoaderImpl. It's a bit of a hack
+     * but we can't pass in 'this' to super constructors.
+     */
+    SimpleQueryImpl(LoaderImpl<?> loader) {
+        this.loader = loader == null ? (LoaderImpl<?>) this : loader;
+    }
 
-	/**
-	 * Create an initial query object; for a typed query this will have a class, otherwise it will be generic.
-	 * For the real QueryImpl itself this is a clone() operation.
-	 */
-	abstract QueryImpl<T> createQuery();
+    /**
+     * Create an initial query object; for a typed query this will have a class, otherwise it will be generic.
+     * For the real QueryImpl itself this is a clone() operation.
+     */
+    abstract QueryImpl<T> createQuery();
 
-	/* (non-Javadoc)
-	 * @see com.googlecode.objectify.cmd.QueryCommon#filterKey(java.lang.String, java.lang.Object)
-	 */
-	@Override
-	public QueryImpl<T> filterKey(String condition, Object value)
-	{
-		QueryImpl<T> q = createQuery();
-		q.addFilter(Entity.KEY_RESERVED_PROPERTY + " " + condition, value);
-		return q;
-	}
+    /* (non-Javadoc)
+     * @see com.googlecode.objectify.cmd.QueryCommon#filterKey(java.lang.String, java.lang.Object)
+     */
+    @Override
+    public QueryImpl<T> filterKey(String condition, Object value) {
+        QueryImpl<T> q = createQuery();
+        q.addFilter(Entity.KEY_RESERVED_PROPERTY + " " + condition, value);
+        return q;
+    }
 
-	/* (non-Javadoc)
-	 * @see com.googlecode.objectify.cmd.SimpleQuery#filterKey(java.lang.Object)
-	 */
-	@Override
-	public QueryImpl<T> filterKey(Object value)
-	{
-		return filterKey("=", value);
-	}
+    /* (non-Javadoc)
+     * @see com.googlecode.objectify.cmd.SimpleQuery#filterKey(java.lang.Object)
+     */
+    @Override
+    public QueryImpl<T> filterKey(Object value) {
+        return filterKey("=", value);
+    }
 
-	/* (non-Javadoc)
-	 * @see com.googlecode.objectify.cmd.Query#ancestor(java.lang.Object)
-	 */
-	@Override
-	public QueryImpl<T> ancestor(Object keyOrEntity)
-	{
-		QueryImpl<T> q = createQuery();
-		q.setAncestor(keyOrEntity);
-		return q;
-	}
+    /* (non-Javadoc)
+     * @see com.googlecode.objectify.cmd.Query#ancestor(java.lang.Object)
+     */
+    @Override
+    public QueryImpl<T> ancestor(Object keyOrEntity) {
+        QueryImpl<T> q = createQuery();
+        q.setAncestor(keyOrEntity);
+        return q;
+    }
 
-	/* (non-Javadoc)
-	 * @see com.googlecode.objectify.cmd.Query#limit(int)
-	 */
-	@Override
-	public QueryImpl<T> limit(int value)
-	{
-		QueryImpl<T> q = createQuery();
-		q.setLimit(value);
-		return q;
-	}
+    /* (non-Javadoc)
+     * @see com.googlecode.objectify.cmd.Query#limit(int)
+     */
+    @Override
+    public QueryImpl<T> limit(int value) {
+        QueryImpl<T> q = createQuery();
+        q.setLimit(value);
+        return q;
+    }
 
-	/* (non-Javadoc)
-	 * @see com.googlecode.objectify.cmd.Query#offset(int)
-	 */
-	@Override
-	public QueryImpl<T> offset(int value)
-	{
-		QueryImpl<T> q = createQuery();
-		q.setOffset(value);
-		return q;
-	}
+    /* (non-Javadoc)
+     * @see com.googlecode.objectify.cmd.Query#offset(int)
+     */
+    @Override
+    public QueryImpl<T> offset(int value) {
+        QueryImpl<T> q = createQuery();
+        q.setOffset(value);
+        return q;
+    }
 
-	/* (non-Javadoc)
-	 * @see com.googlecode.objectify.cmd.Query#startCursor(com.google.appengine.api.datastore.Cursor)
+    /* (non-Javadoc)
+     * @see com.googlecode.objectify.cmd.Query#reverse()
 	 */
-	@Override
-	public QueryImpl<T> startAt(Cursor value)
-	{
-		QueryImpl<T> q = createQuery();
-		q.setStartCursor(value);
-		return q;
-	}
+    @Override
+    public QueryImpl<T> reverse() {
+        QueryImpl<T> q = createQuery();
+        q.setReverse();
+        return q;
+    }
 
-	/* (non-Javadoc)
-	 * @see com.googlecode.objectify.cmd.Query#endCursor(com.google.appengine.api.datastore.Cursor)
-	 */
-	@Override
-	public QueryImpl<T> endAt(Cursor value)
-	{
-		QueryImpl<T> q = createQuery();
-		q.setEndCursor(value);
-		return q;
-	}
+    /* (non-Javadoc)
+         * @see com.googlecode.objectify.cmd.Query#startCursor(com.google.appengine.api.datastore.Cursor)
+         */
+    @Override
+    public QueryImpl<T> startAt(Cursor value) {
+        QueryImpl<T> q = createQuery();
+        q.setStartCursor(value);
+        return q;
+    }
 
-	/* (non-Javadoc)
-	 * @see com.googlecode.objectify.cmd.Query#chunk(int)
-	 */
-	@Override
-	public QueryImpl<T> chunk(int value)
-	{
-		QueryImpl<T> q = createQuery();
-		q.setChunk(value);
-		return q;
-	}
+    /* (non-Javadoc)
+     * @see com.googlecode.objectify.cmd.Query#endCursor(com.google.appengine.api.datastore.Cursor)
+     */
+    @Override
+    public QueryImpl<T> endAt(Cursor value) {
+        QueryImpl<T> q = createQuery();
+        q.setEndCursor(value);
+        return q;
+    }
 
-	/* (non-Javadoc)
-	 * @see com.googlecode.objectify.cmd.SimpleQuery#distinct(boolean)
-	 */
-	@Override
-	public QueryImpl<T> distinct(boolean value)
-	{
-		QueryImpl<T> q = createQuery();
-		q.setDistinct(value);
-		return q;
-	}
+    /* (non-Javadoc)
+     * @see com.googlecode.objectify.cmd.Query#chunk(int)
+     */
+    @Override
+    public QueryImpl<T> chunk(int value) {
+        QueryImpl<T> q = createQuery();
+        q.setChunk(value);
+        return q;
+    }
 
-	/* (non-Javadoc)
-	 * @see com.googlecode.objectify.cmd.SimpleQuery#hybrid(boolean)
-	 */
-	@Override
-	public QueryImpl<T> hybrid(boolean force)
-	{
-		QueryImpl<T> q = createQuery();
-		q.setHybrid(force);
-		return q;
-	}
+    /* (non-Javadoc)
+     * @see com.googlecode.objectify.cmd.SimpleQuery#distinct(boolean)
+     */
+    @Override
+    public QueryImpl<T> distinct(boolean value) {
+        QueryImpl<T> q = createQuery();
+        q.setDistinct(value);
+        return q;
+    }
 
-	/* (non-Javadoc)
-	 * @see com.googlecode.objectify.cmd.SimpleQuery#chunkAll()
-	 */
-	@Override
-	public QueryImpl<T> chunkAll()
-	{
-		return chunk(Integer.MAX_VALUE);
-	}
+    /* (non-Javadoc)
+     * @see com.googlecode.objectify.cmd.SimpleQuery#hybrid(boolean)
+     */
+    @Override
+    public QueryImpl<T> hybrid(boolean force) {
+        QueryImpl<T> q = createQuery();
+        q.setHybrid(force);
+        return q;
+    }
 
-	/* (non-Javadoc)
-	 * @see com.googlecode.objectify.cmd.Query#keys()
-	 */
-	@Override
-	public QueryKeys<T> keys()
-	{
-		QueryImpl<T> q = createQuery();
-		q.setKeysOnly();
-		return new QueryKeysImpl<T>(q);
-	}
+    /* (non-Javadoc)
+     * @see com.googlecode.objectify.cmd.SimpleQuery#chunkAll()
+     */
+    @Override
+    public QueryImpl<T> chunkAll() {
+        return chunk(Integer.MAX_VALUE);
+    }
+
+    /* (non-Javadoc)
+     * @see com.googlecode.objectify.cmd.Query#keys()
+     */
+    @Override
+    public QueryKeys<T> keys() {
+        QueryImpl<T> q = createQuery();
+        q.setKeysOnly();
+        return new QueryKeysImpl<T>(q);
+    }
 }
--- src/test/java/com/googlecode/objectify/test/QueryCursorTestsSmall.java
+++ src/test/java/com/googlecode/objectify/test/QueryCursorTestsSmall.java
@@ -3,137 +3,162 @@
 
 package com.googlecode.objectify.test;
 
-import static com.googlecode.objectify.test.util.TestObjectifyService.fact;
-import static com.googlecode.objectify.test.util.TestObjectifyService.ofy;
+import com.google.appengine.api.datastore.*;
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.cmd.Query;
+import com.googlecode.objectify.test.entity.Trivial;
+import com.googlecode.objectify.test.util.TestBase;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import com.google.appengine.api.datastore.Cursor;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.FetchOptions;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.QueryResultIterator;
-import com.googlecode.objectify.Key;
-import com.googlecode.objectify.cmd.Query;
-import com.googlecode.objectify.test.entity.Trivial;
-import com.googlecode.objectify.test.util.TestBase;
+import static com.googlecode.objectify.test.util.TestObjectifyService.fact;
+import static com.googlecode.objectify.test.util.TestObjectifyService.ofy;
 
 /**
  * Tests of query cursors using a setup of just a couple items.
  *
  * @author Jeff Schnitzer <jeff@infohazard.org>
  */
-public class QueryCursorTestsSmall extends TestBase
-{
-	/** */
-	@SuppressWarnings("unused")
-	private static Logger log = Logger.getLogger(QueryCursorTestsSmall.class.getName());
+public class QueryCursorTestsSmall extends TestBase {
+    /** */
+    @SuppressWarnings("unused")
+    private static Logger log = Logger.getLogger(QueryCursorTestsSmall.class.getName());
 
-	/** */
-	Trivial triv1;
-	Trivial triv2;
-	List<Key<Trivial>> keys;
+    /** */
+    Trivial triv1;
+    Trivial triv2;
+    List<Key<Trivial>> keys;
 
-	/** */
-	@BeforeMethod
-	public void setUp() {
-		super.setUp();
+    /** */
+    @BeforeMethod
+    public void setUp() {
+        super.setUp();
 
-		fact().register(Trivial.class);
+        fact().register(Trivial.class);
 
-		this.triv1 = new Trivial("foo1", 1);
-		this.triv2 = new Trivial("foo2", 2);
+        this.triv1 = new Trivial("foo1", 1);
+        this.triv2 = new Trivial("foo2", 2);
 
-		Map<Key<Trivial>, Trivial> saved = ofy().save().entities(triv1, triv2).now();
+        Map<Key<Trivial>, Trivial> saved = ofy().save().entities(triv1, triv2).now();
 
-		this.keys = new ArrayList<Key<Trivial>>(saved.keySet());
-	}
+        this.keys = new ArrayList<Key<Trivial>>(saved.keySet());
+    }
 
-	/** */
-	@Test
-	public void testCursorEnd() throws Exception {
-		Query<Trivial> q = ofy().load().type(Trivial.class);
-		QueryResultIterator<Trivial> it = q.limit(1).iterator();
+    /** */
+    @Test
+    public void testCursorEnd() throws Exception {
+        Query<Trivial> q = ofy().load().type(Trivial.class);
+        QueryResultIterator<Trivial> it = q.limit(1).iterator();
 
-		assert it.hasNext();
-		Trivial t1 = it.next();
-		assert t1.getId().equals(triv1.getId());
-		assert !it.hasNext();
+        assert it.hasNext();
+        Trivial t1 = it.next();
+        assert t1.getId().equals(triv1.getId());
+        assert !it.hasNext();
 
-		Cursor cursor = it.getCursor();
-		assert cursor != null;
+        Cursor cursor = it.getCursor();
+        assert cursor != null;
 
-		it = q.startAt(cursor).limit(1).iterator();
+        it = q.startAt(cursor).limit(1).iterator();
 
-		assert it.hasNext();
-		Trivial t2 = it.next();
-		assert t2.getId().equals(triv2.getId());
-		assert !it.hasNext();
+        assert it.hasNext();
+        Trivial t2 = it.next();
+        assert t2.getId().equals(triv2.getId());
+        assert !it.hasNext();
 
-		// We should be at end
-		cursor = it.getCursor();
-		assert cursor != null;
-		it = q.startAt(cursor).iterator();
-		assert !it.hasNext();
+        // We should be at end
+        cursor = it.getCursor();
+        assert cursor != null;
+        it = q.startAt(cursor).iterator();
+        assert !it.hasNext();
 
-		// Try that again just to be sure
-		cursor = it.getCursor();
-		assert cursor != null;
-		it = q.startAt(cursor).iterator();
-		assert !it.hasNext();
-	}
+        // Try that again just to be sure
+        cursor = it.getCursor();
+        assert cursor != null;
+        it = q.startAt(cursor).iterator();
+        assert !it.hasNext();
+    }
 
-	/** */
-	@Test
-	public void testCursorEndLowLevelBehavior() throws Exception {
-		com.google.appengine.api.datastore.Query query = new com.google.appengine.api.datastore.Query("Trivial");
-		PreparedQuery pq = ds().prepare(query);
+    /** */
+    @Test
+    public void testCursorEndLowLevelBehavior() throws Exception {
+        com.google.appengine.api.datastore.Query query = new com.google.appengine.api.datastore.Query("Trivial");
+        PreparedQuery pq = ds().prepare(query);
 
-		QueryResultIterator<Entity> it = pq.asQueryResultIterable().iterator();
-		it.next();
-		it.next();
-		assert !it.hasNext();
+        QueryResultIterator<Entity> it = pq.asQueryResultIterable().iterator();
+        it.next();
+        it.next();
+        assert !it.hasNext();
 
-		Cursor cursor = it.getCursor();
-		assert cursor != null;
+        Cursor cursor = it.getCursor();
+        assert cursor != null;
 
-		QueryResultIterator<Entity> it2 = pq.asQueryResultIterable(FetchOptions.Builder.withStartCursor(cursor)).iterator();
-		assert !it2.hasNext();
+        QueryResultIterator<Entity> it2 = pq.asQueryResultIterable(FetchOptions.Builder.withStartCursor(cursor)).iterator();
+        assert !it2.hasNext();
 
-		Cursor cursor2 = it2.getCursor();
-		assert cursor2 != null;
+        Cursor cursor2 = it2.getCursor();
+        assert cursor2 != null;
 
-		QueryResultIterator<Entity> it3 = pq.asQueryResultIterable(FetchOptions.Builder.withStartCursor(cursor2)).iterator();
-		assert !it3.hasNext();
-		assert it3.getCursor() != null;
-	}
+        QueryResultIterator<Entity> it3 = pq.asQueryResultIterable(FetchOptions.Builder.withStartCursor(cursor2)).iterator();
+        assert !it3.hasNext();
+        assert it3.getCursor() != null;
+    }
 
-	/** */
-	@Test
-	public void testCursorOneFetchToEnd() throws Exception {
-		Query<Trivial> q = ofy().load().type(Trivial.class);
-		QueryResultIterator<Trivial> it = q.iterator();
+    @Test
+    public void testCursorReverse() throws Exception {
+        Query<Trivial> q = ofy().load().type(Trivial.class).order("id");
+        QueryResultIterator<Trivial> it = q.iterator();
 
-		it.next();
-		it.next();
-		assert !it.hasNext();
+        Cursor cursor0 = it.getCursor();
+        final Trivial item1 = it.next();
 
-		// We should be at end
-		Cursor cursor = it.getCursor();
-		assert cursor != null;
-		it = q.startAt(cursor).iterator();
-		assert !it.hasNext();
+        Cursor cursor1 = it.getCursor();
+        final Trivial item2 = it.next();
+        assert !it.hasNext();
 
-		// Try that again just to be sure
-		cursor = it.getCursor();
-		assert cursor != null;
-		it = q.startAt(cursor).iterator();
-		assert !it.hasNext();
-	}
+        Cursor cursor2 = it.getCursor();
+        Cursor cursor2Rev = it.getCursor().reverse();
+
+        it = q.reverse().startAt(cursor2Rev).iterator();
+
+        final Trivial item2Rev = it.next();
+
+        assert it.getCursor().equals(cursor2);
+        assert item2Rev.getSomeString().equals(item2.getSomeString());
+
+        assert it.hasNext();
+
+        final Trivial item1Rev = it.next();
+
+        assert it.getCursor().equals(cursor1);
+        assert item1Rev.getSomeString().equals(item1.getSomeString());
+        assert !it.hasNext();
+    }
+
+    /** */
+    @Test
+    public void testCursorOneFetchToEnd() throws Exception {
+        Query<Trivial> q = ofy().load().type(Trivial.class);
+        QueryResultIterator<Trivial> it = q.iterator();
+
+        it.next();
+        it.next();
+        assert !it.hasNext();
+
+        // We should be at end
+        Cursor cursor = it.getCursor();
+        assert cursor != null;
+        it = q.startAt(cursor).iterator();
+        assert !it.hasNext();
+
+        // Try that again just to be sure
+        cursor = it.getCursor();
+        assert cursor != null;
+        it = q.startAt(cursor).iterator();
+        assert !it.hasNext();
+    }
 }
