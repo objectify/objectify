23d1a5c24da71b3be68e1dfc545cb5fcd0fb0505
Jeff Schnitzer <jeff@infohazard.org>
2014-04-08 17:17:07 -0700

    Added test for lifecycle methods in embedded classes

---
+++ src/test/java/com/googlecode/objectify/test/EmbeddedLifecycleTests.java
@@ -0,0 +1,52 @@
+package com.googlecode.objectify.test;
+
+import com.googlecode.objectify.Ref;
+import com.googlecode.objectify.SaveException;
+import com.googlecode.objectify.annotation.Cache;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Ignore;
+import com.googlecode.objectify.annotation.Load;
+import com.googlecode.objectify.annotation.OnLoad;
+import com.googlecode.objectify.annotation.OnSave;
+import com.googlecode.objectify.annotation.Parent;
+import com.googlecode.objectify.test.entity.Trivial;
+import com.googlecode.objectify.test.util.TestBase;
+import org.testng.annotations.Test;
+
+import static com.googlecode.objectify.test.util.TestObjectifyService.fact;
+import static com.googlecode.objectify.test.util.TestObjectifyService.ofy;
+
+/**
+ * Tests the lifecycle annotations on embedded classes
+ */
+public class EmbeddedLifecycleTests extends TestBase
+{
+	@com.googlecode.objectify.annotation.Entity
+	@Cache
+	public static class Outer {
+		@Id Long id;
+		HasLifecycle life;
+	}
+
+	public static class HasLifecycle {
+		@Ignore boolean onSaved;
+		@Ignore boolean onLoaded;
+
+		@OnSave void onSave() { this.onSaved = true; }
+		@OnLoad void onLoad() { this.onLoaded = true; }
+	}
+
+	/** */
+	@Test
+	public void lifecycleInEmbeddedClassWorks() throws Exception {
+		fact().register(Outer.class);
+
+		Outer outer = new Outer();
+		outer.life = new HasLifecycle();
+
+		Outer fetched = ofy().saveClearLoad(outer);
+
+		assert outer.life.onSaved;
+		assert fetched.life.onLoaded;	// would fail without session clear
+	}
+}
