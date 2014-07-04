a7e64885f2534c7e24afc1d8259aa8c8bdeed0a2
Ariel Haviv <ariel.haviv@gmail.com>
2014-07-04 12:23:24 +0300

    typo

--- src/main/java/com/googlecode/objectify/annotation/Container.java
+++ src/main/java/com/googlecode/objectify/annotation/Container.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * <p>Place on one or more fields insde an embedded class. When loaded, the field will be populated
+ * <p>Place on one or more fields inside an embedded class. When loaded, the field will be populated
  * with a reference to the containing object. Since embedded classes can nest other classes, this will reference
  * the first object of the correct type when searching up the chain.</p>
  * 
