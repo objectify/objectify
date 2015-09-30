7d5a461c5b4d2c0e799365ec6a363b6d03cc9453
Jeff Schnitzer <jeff@infohazard.org>
2015-09-30 12:06:29 -0700

    Add LoadUnless test for collection field

--- src/test/java/com/googlecode/objectify/test/LoadUnlessTests.java
+++ src/test/java/com/googlecode/objectify/test/LoadUnlessTests.java
@@ -3,8 +3,6 @@
 
 package com.googlecode.objectify.test;
 
-import org.testng.annotations.Test;
-
 import com.googlecode.objectify.Key;
 import com.googlecode.objectify.Ref;
 import com.googlecode.objectify.annotation.Entity;
@@ -13,9 +11,14 @@ import com.googlecode.objectify.annotation.Load;
 import com.googlecode.objectify.test.LoadUnlessTests.One.No;
 import com.googlecode.objectify.test.LoadUnlessTests.One.Yes;
 import com.googlecode.objectify.test.util.TestBase;
-
+import org.testng.annotations.Test;
+import java.util.ArrayList;
+import java.util.List;
 import static com.googlecode.objectify.test.util.TestObjectifyService.fact;
 import static com.googlecode.objectify.test.util.TestObjectifyService.ofy;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 
 /**
  * Tests of @Load(unless=Blah.class)
@@ -43,8 +46,7 @@ public class LoadUnlessTests extends TestBase
 
 	/** */
 	@Test
-	public void testLoadUnless() throws Exception
-	{
+	public void testLoadUnless() throws Exception {
 		fact().register(One.class);
 		fact().register(Two.class);
 
@@ -83,5 +85,49 @@ public class LoadUnlessTests extends TestBase
 		One fetchedYesNo = ofy().load().group(Yes.class, No.class).entity(one).now();
 		assert !fetchedYesNo.always.isLoaded();
 		assert !fetchedYesNo.sometimes.isLoaded();
+	}
+
+	/** */
+	@Entity
+	public static class Multi {
+		public @Id long id;
+		public @Load(unless=No.class) List<Ref<Two>> always = new ArrayList<>();
+	}
+
+	/** */
+	@Test
+	public void testLoadUnlessMulti() throws Exception {
+		fact().register(Multi.class);
+		fact().register(Two.class);
+
+		Two two1 = new Two();
+		two1.id = 456;
+		Key<Two> two1Key = ofy().save().entity(two1).now();
+		Ref<Two> two1Ref = Ref.create(two1Key);
+
+		Two two2 = new Two();
+		two2.id = 789;
+		Key<Two> two2Key = ofy().save().entity(two2).now();
+		Ref<Two> two2Ref = Ref.create(two2Key);
+
+		Multi multi = new Multi();
+		multi.id = 123;
+		multi.always.add(two1Ref);
+		multi.always.add(two2Ref);
+		ofy().save().entity(multi).now();
+
+		ofy().clear();
+		Multi fetchedDefault = ofy().load().entity(multi).now();
+		assertThat(fetchedDefault.always, hasSize(2));
+		for (Ref<Two> ref : fetchedDefault.always) {
+			assertThat(ref.isLoaded(), equalTo(true));
+		}
+
+		ofy().clear();
+		Multi fetchedNo = ofy().load().group(No.class).entity(multi).now();
+		assertThat(fetchedNo.always, hasSize(2));
+		for (Ref<Two> ref : fetchedNo.always) {
+			assertThat(ref.isLoaded(), equalTo(false));
+		}
 	}
 }
