1d5047d28a16e0ff8f6c4b1ca0836093320ead47
Yan Zhao <zhaoyan1117@gmail.com>
2017-02-12 21:47:35 -0800

    anonymous class

--- src/main/java/com/googlecode/objectify/cache/MemcacheServiceFactory.java
+++ src/main/java/com/googlecode/objectify/cache/MemcacheServiceFactory.java
@@ -9,14 +9,12 @@ import com.google.appengine.api.memcache.MemcacheService;
  * @author Yan Zhao <zhaoyan1117@gmail.com>
  */
 public interface MemcacheServiceFactory {
-	MemcacheServiceFactory DEFAULT = new DefaultMemcacheServiceFactory();
-
 	MemcacheService getMemcacheService(String namespace);
 
-	class DefaultMemcacheServiceFactory implements MemcacheServiceFactory {
+	MemcacheServiceFactory DEFAULT = new MemcacheServiceFactory() {
 		@Override
 		public MemcacheService getMemcacheService(String namespace) {
 			return com.google.appengine.api.memcache.MemcacheServiceFactory.getMemcacheService(namespace);
 		}
-	}
+	};
 }
