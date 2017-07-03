2d3ebc00ad78c7e0cec84b81a196c0d2140e2d29
Jeff Schnitzer <jeff@infohazard.org>
2017-07-03 10:04:20 -0700

    Add happy path tests for mandatoryTransactions()

--- src/test/java/com/googlecode/objectify/test/MandatoryTransactionsTests.java
+++ src/test/java/com/googlecode/objectify/test/MandatoryTransactionsTests.java
@@ -33,4 +33,31 @@ public class MandatoryTransactionsTests extends TestBase
 
 		ofy().mandatoryTransactions(true).delete().type(Trivial.class).id(123L).now();
 	}
+
+	/** */
+	@Test
+	public void mandatoryTransactionsWorkingForSave() throws Exception {
+		fact().register(Trivial.class);
+
+		ofy().transact(new Runnable() {
+			@Override
+			public void run() {
+				final Trivial triv = new Trivial();
+				ofy().mandatoryTransactions(true).save().entity(triv).now();
+			}
+		});
+	}
+
+	/** */
+	@Test
+	public void mandatoryTransactionsWorkingForDelete() throws Exception {
+		fact().register(Trivial.class);
+
+		ofy().transact(new Runnable() {
+			@Override
+			public void run() {
+				ofy().mandatoryTransactions(true).delete().type(Trivial.class).id(123L).now();
+			}
+		});
+	}
 }
