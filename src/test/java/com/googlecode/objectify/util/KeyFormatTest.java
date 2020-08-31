2ceee95d115947dd5fedbfd629e86c1301d82d12
Jeff Schnitzer <jeff@infohazard.org>
2020-08-31 12:54:44 -0700

    Fix KeyFormatTest after new encoding changes

--- src/test/java/com/googlecode/objectify/util/KeyFormatTest.java
+++ src/test/java/com/googlecode/objectify/util/KeyFormatTest.java
@@ -8,11 +8,16 @@ import static com.google.common.truth.Truth.assertThat;
 class KeyFormatTest {
 	@Test
 	void parsesAndFormatsOldStyleGAEKey() throws Exception {
-		final String urlsafeKey = "agxzfm1haWxmb29nYWVyKAsSDE9yZ2FuaXphdGlvbiIKc3RyZWFrLmNvbQwLEgRVc2VyGJneWww";
+		// Both of these get converted to the second form
+		final String urlsafeKey1 = "agxzfm1haWxmb29nYWVyKAsSDE9yZ2FuaXphdGlvbiIKc3RyZWFrLmNvbQwLEgRVc2VyGJneWww";
+		final String urlsafeKey2 = "agxzfm1haWxmb29nYWVyKAsSDE9yZ2FuaXphdGlvbiIKc3RyZWFrLmNvbQwLEgRVc2VyGJneWwyiAQA";
 
-		final Key key = KeyFormat.INSTANCE.parseOldStyleAppEngineKey(urlsafeKey);
+		final Key key = KeyFormat.INSTANCE.parseOldStyleAppEngineKey(urlsafeKey1);
 		final String restringified = KeyFormat.INSTANCE.formatOldStyleAppEngineKey(key);
+		assertThat(restringified).isEqualTo(urlsafeKey2);
 
-		assertThat(restringified).isEqualTo(urlsafeKey);
+		final Key key2 = KeyFormat.INSTANCE.parseOldStyleAppEngineKey(urlsafeKey2);
+		final String restringified2 = KeyFormat.INSTANCE.formatOldStyleAppEngineKey(key2);
+		assertThat(restringified2).isEqualTo(urlsafeKey2);
 	}
 }
