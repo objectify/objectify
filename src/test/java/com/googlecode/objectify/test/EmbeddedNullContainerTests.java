e4ec80ce0a1fcf69228f3997a11bd30ddd6a4887
jimit-j-shah <57637300+jimit-j-shah@users.noreply.github.com>
2024-06-02 23:34:06 -0700

    EmbeddedMapNullContainerTest

--- src/test/java/com/googlecode/objectify/test/EmbeddedNullContainerTests.java
+++ src/test/java/com/googlecode/objectify/test/EmbeddedNullContainerTests.java
@@ -1,2 +1,77 @@
-package com.googlecode.objectify.test;public class EmbeddedNullContainerTests {
+package com.googlecode.objectify.test;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.test.util.TestBase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.googlecode.objectify.ObjectifyService.factory;
+
+public class EmbeddedNullContainerTests extends TestBase {
+
+  @Test
+  void testLoadingEmbeddedMapNullValue() {
+    factory().register(Sample.class);
+
+    List<Value> valueList = new ArrayList<>();
+    valueList.add(new Value(null, null));
+
+    Map<String, List<Value>> values = new HashMap<String, List<Value>>();
+    values.put("k1", valueList);
+
+    final Sample sample = new Sample("testKey", values);
+    final Sample retrieved  = saveClearLoad(sample);
+
+    assertThat(sample.values).isEqualTo(retrieved.values);
+  }
+
+
+  @Ignore ("This test fails as follows:"
+      + "unexpected keys\n"
+      + "for key         : k1\n"
+      + "unexpected value: null\n"
+      + "---\n"
+      + "expected        : {}\n"
+      + "but was         : {k1=null}\n")
+  @Test
+  void testLoadingNullListValue() {
+    factory().register(Sample.class);
+
+    Map<String, List<Value>> values = new HashMap<String, List<Value>>();
+    values.put("k1", null);
+
+    final Sample sample = new Sample("testKey", values);
+    final Sample retrieved  = saveClearLoad(sample);
+
+    assertThat(sample.values).isEqualTo(retrieved.values);
+  }
+
+
+  @Entity(name = "Sample")
+  @Data
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class Sample {
+    @Id
+    String name;
+    Map<String, List<Value>> values;
+  }
+
+  @Data
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class Value {
+    String primitiveField;
+    Map<String, String> structuredField;
+  }
 }
